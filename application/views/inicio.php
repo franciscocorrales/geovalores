74c38565f19f61be8e5fc43b5a256dec2fdcd42e
laqs84 <laqs84@softteca.com>
2014-11-01 11:22:28 -0600

    prueba

---
+++ application/views/inicio.php
@@ -0,0 +1,223 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>GeoValores</title>
+
+	
+</head>
+<body>
+
+	<!-- header -->
+<div id="headerframe">
+	<div id="header">
+		<div id="header-logo">
+			<a href="/"><img src="images/logo.png" border="0" align="left" class="logo" /></a>
+		</div>
+		<!-- main nav -->
+		<div id="mainnavigation">
+			<div id="nav-gutter">
+				<ul id="nav" class="mainmenu">
+					<li class="mainmenu-item mainmenu-item-5346 active first"><a href="/"><span>Home</span></a></li>
+					<li class="mainmenu-item mainmenu-item-5380  "><a href="/portfolio/"><span>Portfolio</span></a></li>
+					<li class="mainmenu-item mainmenu-item-5381  "><a href="/packages/"><span>Packages</span></a></li>
+					<li class="mainmenu-item mainmenu-item-5441  "><a href="/services/"><span>Services</span></a></li>
+					<li class="mainmenu-item mainmenu-item-5357  "><a href="/about-us/"><span>About&nbsp;Us</span></a></li>
+					<li class="mainmenu-item mainmenu-item-5382  last"><a href="/contact-us/"><span>Contact&nbsp;Us</span></a></li>
+				</ul>
+				<div style="clear: left"></div>
+			</div>
+		</div>
+                <div id="responsive-nav-button" style="display: none">
+			<div>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+		<div id="responsive-navigation" style="display: none">
+			<div class="padding">
+				<ul id="responsive-nav" class="responsivemenu">
+					<li class="responsivemenu-item responsivemenu-item-5346 active first"><a href="/"><span>Home</span></a></li>
+					<li class="responsivemenu-item responsivemenu-item-5380  "><a href="/portfolio/"><span>Portfolio</span></a></li>
+					<li class="responsivemenu-item responsivemenu-item-5381  "><a href="/packages/"><span>Packages</span></a></li>
+					<li class="responsivemenu-item responsivemenu-item-5441  "><a href="/services/"><span>Services</span></a></li>
+					<li class="responsivemenu-item responsivemenu-item-5357  "><a href="/about-us/"><span>About&nbsp;Us</span></a></li>
+					<li class="responsivemenu-item responsivemenu-item-5382  last"><a href="/contact-us/"><span>Contact&nbsp;Us</span></a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="social-header">
+			<div id="fb-header"><a target="_blank" href="#"><img src="/img/badge-facebook.png" border="0"/></a></div>
+		    <div id="twitter-header"><a target="_blank" href="#"><img src="/img/badge-twitter.png" border="0"/></a></div>
+		    <div id="youtube-header"><a target="_blank" href="#"><img src="/img/badge-youtube.png" border="0"/></a></div>
+		</div>
+	</div>
+</div>
+	<!-- content -->
+<div id="siteframe">
+	<div id="content">
+		<div class="content-padding">
+                    <div class="content-left">
+                        <div class="padding-left">
+                            <div class="nav-content-left">
+                                <ul>
+                                    <li class="mainmenu-item"><a href="/"><span>Home</span></a></li>
+                                    <li class="mainmenu-item"><a href="/portfolio/"><span>Portfolio</span></a></li>
+                                    <li class="mainmenu-item"><a href="/packages/"><span>Packages</span></a></li>
+                                    <li class="mainmenu-item"><a href="/services/"><span>Services</span></a></li>
+                                    <li class="mainmenu-item"><a href="/about-us/"><span>About&nbsp;Us</span></a></li>
+                                    <li class="mainmenu-item"><a href="/contact-us/"><span>Contact&nbsp;Us</span></a></li>
+                                </ul>
+                            </div>
+
+                            <div class="moreNews">
+                                <div class="titles">
+                                    <h3>Noticias y Eventos</h3>
+                                </div>
+                                <div class="news">
+                                   <div class="newDate">
+                                        <h4>21/6/2014</h4>
+                                    </div>
+                                    <div class="newContent">
+                                        <p>En primer lugar En esta publicidad es la necesidad de cualquier organización</p>
+                                    </div>
+                                    <div class="read-more">
+                                        <a href="" ><span>leer Más</span></a>
+                                    </div>
+                                </div>
+                                <div class="news">
+                                   <div class="newDate">
+                                        <h4>21/6/2014</h4>
+                                    </div>
+                                    <div class="newContent">
+                                        <p>En primer lugar En esta publicidad es la necesidad de cualquier organización</p>
+                                    </div>
+                                    <div class="read-more">
+                                        <a href="" ><span>leer Más</span></a>
+                                    </div>
+                                </div>
+                                <div class="news">
+                                   <div class="newDate">
+                                        <h4>21/6/2014</h4>
+                                    </div>
+                                    <div class="newContent">
+                                        <p>En primer lugar En esta publicidad es la necesidad de cualquier organización</p>
+                                    </div>
+                                    <div class="read-more">
+                                        <a href="" ><span>leer Más</span></a>
+                                    </div>
+                                </div>
+                                <div class="news">
+                                   <div class="newDate">
+                                        <h4>21/6/2014</h4>
+                                    </div>
+                                    <div class="newContent">
+                                        <p>En primer lugar En esta publicidad es la necesidad de cualquier organización</p>
+                                    </div>
+                                    <div class="read-more">
+                                        <a href="" ><span>leer Más</span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>   
+                    </div>
+                    <div class="content-rigth">
+                        <div id="google_map"></div>
+                        <div class="content-search">
+                            <div class="stand-out content-description">
+                                <div class="titles">
+                                    <h3>Casas Destacadas</h3>
+                                </div>
+                                <div class='stand-house'>
+                                    <img src="images/house.png" border="0"/>
+                                </div>
+                                <div class="description-house">
+                                    <p>En primer lugar En esta publicidad es la necesidad de cualquier organización a la fama de su empresa en el mundo. Nuestro Café es en esta época. En esta publicidad es la necesidad.... </p>
+                                </div>
+                                <div class="read-more">
+                                    <a href="" ><span>leer Más</span></a>
+                                </div>
+                            </div>
+                            <div class="form-search content-description">
+                                <form action="" method="">
+                                    <ul>
+                                        <li>
+                                             <h2>Busqueda</h2>
+                                             
+                                        </li>
+                                        <li>
+                                            <label for="name" >Ciudad:</label>
+                                            <input type="text" name="ciudad" />
+                                        </li>
+                                        <li>
+                                            <label for="name" >Precio:</label>
+                                            <input type="text" name="precio" />
+                                        </li>
+                                        <li>
+                                            <label for="name" >Pisos:</label>
+                                            <input type="text" name="pisos" />
+                                        </li>
+                                        
+                                    </ul>
+                                </form>
+                            </div>
+                        </div>        
+                    </div>
+                        
+                    </div>
+	</div>
+</div>
+	<!-- end content -->	
+
+	<!-- footer -->
+<div id="footerframe">
+	<div id="footer">
+		<div id="footer-nav"><a href="/" class="footer-nav-5346 active first"><span>Home</span></a>&nbsp;<span class="nav-divider">|</span>&nbsp;<a href="/portfolio/" class="footer-nav-5380  "><span>Portfolio</span></a>&nbsp;<span class="nav-divider">|</span>&nbsp;<a href="/packages/" class="footer-nav-5381  "><span>Packages</span></a>&nbsp;<span class="nav-divider">|</span>&nbsp;<a href="/services/" class="footer-nav-5441  "><span>Services</span></a>&nbsp;<span class="nav-divider">|</span>&nbsp;<a href="/about-us/" class="footer-nav-5357  "><span>About&nbsp;Us</span></a>&nbsp;<span class="nav-divider">|</span>&nbsp;<a href="/contact-us/" class="footer-nav-5382  last"><span>Contact&nbsp;Us</span></a></div>
+		<div id="footer-extra"></div>
+		<p id="copyright"><?= date(y); ?> &copy; GeoValores</p>
+	</div>
+	<!-- end footer -->
+</div>
+<!--  end siteframe -->
+
+</body>
+</html>
